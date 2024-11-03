7e971969673649a1e107e8f3e7a3c2f2a9746cb8
dima bubnov <aspire14880@gmail.com>
2024-11-03 18:49:46 +0300

    lesson 1 - NumberComparer

---
+++ src/NumberComparer.java
@@ -0,0 +1,18 @@
+public class NumberComparer {public static void compareNumbers() {
+    // Объявление и инициализация переменных a и b
+    int a = 5; // Вы можете изменить это значение на любое другое
+    int b = 3; // Вы можете изменить это значение на любое другое
+
+    // Сравнение a и b
+    if (a >= b) {
+        System.out.println("a >= b");
+    } else {
+        System.out.println("a < b");
+    }
+}
+
+    public static void main(String[] args) {
+        // Вызов метода для сравнения чисел
+        compareNumbers();
+    }
+}
